57766aec6fa438ae5704cf53f90e91c04d18d87d
leshayu <alexei.yurovskikh@yandex.ru>
2025-01-05 15:10:38 +0500

    add javadoc

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,3 +1,6 @@
+/**
+ * Класс, представляющий реализацию основных методов ArrayList.
+ */
 public class MyArrayList<T> {
     private Object[] elements;
     private int size;
@@ -7,14 +10,23 @@ public class MyArrayList<T> {
         elements = new Object[DEFAULT_CAPACITY];
         size = 0;
     }
-
+    /**
+     * Добавляет элементы в массив
+     *
+     * @param element элемент который добавляем в массив
+     */
     public void add(T element) {
         if (size == elements.length) {
             increaseCapacity();
         }
         elements[size++] = element;
     }
-
+    /**
+     * Добавляет элементы в массив
+     *
+     * @param index индекс элемента который мы хотим удалить
+     * @return возвращает массив с удаленным элементом по индексу
+     */
     public T remove(int index) {
         checkIndex(index);
         T removedElement = (T) elements[index];
@@ -24,17 +36,33 @@ public class MyArrayList<T> {
         elements[--size] = null;
         return removedElement;
     }
-
+    /**
+     * Получает элемент массива по индексу
+     *
+     * @param index индекс элемента который мы хотим получить
+     * @return возвращает элемент по индексу
+     */
     public T get(int index) {
         checkIndex(index);
         return (T) elements[index];
     }
-
+    /**
+     * Изменяет элемент в массив по индексу
+     *
+     * @param index индекс элемента который мы хотим изменить
+     */
     public void set(int index, T element) {
         checkIndex(index);
         elements[index] = element;
     }
-
+    /**
+     * Возвращает представление части массива между указанными индексами fromIndex (включительно) и toIndex (исключительно)
+     *
+     * @param fromIndex индекс элемента с которого мы хотим получить
+     * @param toIndex индекс элемента по который мы хотим получить
+     * @return возвращает список элементов по указанным индексам
+     * @exception IndexOutOfBoundsException
+     */
     public MyArrayList<T> subList(int fromIndex, int toIndex) {
         if (fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
             throw new IndexOutOfBoundsException("Invalid sublist range");
@@ -45,11 +73,15 @@ public class MyArrayList<T> {
         }
         return subList;
     }
-
+    /**
+     * Возвращает размер массива
+     */
     public int size() {
         return size;
     }
-
+    /**
+     * Увеличивает емкость массива при добавлении новых элементов.
+     */
     private void increaseCapacity() {
         int newCapacity = elements.length * 2;
         Object[] newArray = new Object[newCapacity];
@@ -58,10 +90,13 @@ public class MyArrayList<T> {
         }
         elements = newArray;
     }
-
+    /**
+     * Проверяет, что индекс действителен. Если нет, выбрасывает исключение IndexOutOfBoundsException
+     * @exception IndexOutOfBoundsException
+     */
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+            throw new IndexOutOfBoundsException("Индекс: " + index + ", Размер: " + size);
         }
     }
 }
